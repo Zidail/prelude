dd8c6cc176fe216aa16f76fc90fb6c080e3e1430
Leon Mok <mok.leon@gmail.com>
2018-01-15 16:10:16 +0800

    Use local project eslint instead of global

--- settings/setup-flycheck.el
+++ settings/setup-flycheck.el
@@ -14,4 +14,17 @@
 ;; use eslint with web-mode for jsx files
 (flycheck-add-mode 'javascript-eslint 'web-mode)
 
+;; use project eslint instead of global
+(defun use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'use-eslint-from-node-modules)
+
 (provide 'setup-flycheck)
